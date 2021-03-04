9c7a0b7c52f282c07891ddc8a77347f26b944ede
atayl16 <atayl16@yahoo.com>
2021-03-04 14:39:39 +0100

    styling

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,26 +1,21 @@
 class MerchantsController < ApplicationController
   before_action :set_merchant, only: %i[ show edit update destroy ]
 
-  # GET /merchants or /merchants.json
   def index
     @merchants = Merchant.all
   end
 
-  # GET /merchants/1 or /merchants/1.json
   def show
     @merchant = Merchant.friendly.find(params[:id])
   end
 
-  # GET /merchants/new
   def new
     @merchant = Merchant.new
   end
-
-  # GET /merchants/1/edit
+  
   def edit
   end
 
-  # POST /merchants or /merchants.json
   def create
     @merchant = Merchant.new(merchant_params)
 
@@ -35,7 +30,6 @@ class MerchantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /merchants/1 or /merchants/1.json
   def update
     respond_to do |format|
       if @merchant.update(merchant_params)
@@ -48,7 +42,6 @@ class MerchantsController < ApplicationController
     end
   end
 
-  # DELETE /merchants/1 or /merchants/1.json
   def destroy
     @merchant.destroy
     respond_to do |format|
@@ -66,12 +59,10 @@ class MerchantsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_merchant
       @merchant = Merchant.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def merchant_params
       params.require(:merchant).permit(:name, :booking_widget, :waitlist_widget, :class_widget, :logo, :mid, :slug, :image_url, :sentence, :vertical)
     end
